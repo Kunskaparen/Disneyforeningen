a6eb4601cedeea74a8eb5665472a7cdcb46067f8
Kunskaparen <henrik.akerberg@hotmail.com>
2014-12-03 17:44:06 +0100

    Börjat på lösenord

--- script.js
+++ script.js
@@ -3,3 +3,20 @@
 var ieAlert = function(){
 	alert("Synd, för det får du inte!")
 };
+
+var taUppgifter = function(){ 
+	document.getElementById("submitLogin").addEventlistener("onclick",login,True);
+}
+var login = function(){
+	
+	var loginuppgifter = {"Baloo":"Halabaloo"};
+	for(var användarnamn in loginuppgifter){
+		if (användarnamn.toLowerCase == enteredname.toLowerCase && loginuppgifter[användarnamn] == enteredpassword){
+			var inloggad = True
+			var inloggadsom = användarnamn
+			}
+		}
+	}
+};
+
+window.onload = taUppgifter;
