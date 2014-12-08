8f49372dde59708063c089d407b472bd4fc0deaa
Kunskaparen <henrik.akerberg@hotmail.com>
2014-12-08 13:49:29 +0100

    Javascript har syntax error

--- script.js
+++ script.js
@@ -12,17 +12,27 @@ var login = function(){
 	var enteredPassword = document.getElementById("enteredPassword").value;
 	var loginUppgifter = {"Baloo":"Halabaloo"};
 	for(var användarnamn in loginUppgifter){
-		if (användarnamn.toLowerCase == enteredName.toLowerCase && loginUppgifter[användarnamn] == enteredPassword){
-			var inloggad = true
-			var inloggadSom = användarnamn
-			console.log("Login lyckades")
-			console.log("Inloggad som: "+användarnamn)
-			alert("Du är inloggad som: "+användarnamn)
-			break
+		if enteredName == null || enteredName == undefined{
+			console.log("Inget användarnamn")
+			alert("Skriv in ett användarnamn")
+		}
+		else if enteredPassword == null || enteredPassword == undefined{
+		console.log("Inget lösenord")
+		alert("Skriv in ett användarnamn")
 		}
 		else{
-			console.log("Login misslyckades")
-			alert("Du loggades inte in");
+			if (användarnamn.toLowerCase == enteredName.toLowerCase && loginUppgifter[användarnamn] == enteredPassword){
+				var inloggad = true
+				var inloggadSom = användarnamn
+				console.log("Login lyckades")
+				console.log("Inloggad som: "+användarnamn)
+				alert("Du är inloggad som: "+användarnamn)
+				break
+			}
+			else{
+				console.log("Login misslyckades")
+				alert("Du loggades inte in");
+			}
 		}
 	}
 }
