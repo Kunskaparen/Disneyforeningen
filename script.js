02f836839ce3b0dccaa06f10359c49c0e8ba8de6
Kunskaparen <henrik.akerberg@hotmail.com>
2014-12-20 20:18:43 +0100

    Nu funkar inloggningsskriptet

--- script.js
+++ script.js
@@ -12,28 +12,27 @@ var login = function(){
 	var enteredPassword = document.getElementById("enteredPassword").value;
 	var loginUppgifter = {"Baloo":"Halabaloo"};
 	for(var användarnamn in loginUppgifter){
-		if enteredName == null || enteredName == undefined{
+		if (enteredName == null || enteredName == undefined || enteredName == ""){
 			console.log("Inget användarnamn")
 			alert("Skriv in ett användarnamn")
 		}
-		else if enteredPassword == null || enteredPassword == undefined{
+		else if (enteredPassword == null || enteredPassword == undefined || enteredPassword == ""){
 		console.log("Inget lösenord")
-		alert("Skriv in ett användarnamn")
+		alert("Skriv in ett lösenord")
 		}
-		else{
-			if (användarnamn.toLowerCase == enteredName.toLowerCase && loginUppgifter[användarnamn] == enteredPassword){
+		else if (användarnamn.toLowerCase == enteredName.toLowerCase && loginUppgifter[användarnamn] == enteredPassword){
 				var inloggad = true
 				var inloggadSom = användarnamn
 				console.log("Login lyckades")
 				console.log("Inloggad som: "+användarnamn)
 				alert("Du är inloggad som: "+användarnamn)
 				break
-			}
-			else{
+		}
+		else{
 				console.log("Login misslyckades")
 				alert("Du loggades inte in");
-			}
 		}
 	}
 }
+
 window.onload = taUppgifter;
